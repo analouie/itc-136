c358271fd6179cf2d8f77ef0a2462a6dcfa085b5
analouie <analia.ayla@gmail.com>
2018-10-29 20:46:48 -0700

    Added new echo commands

--- my_script.sh
+++ my_script.sh
@@ -4,3 +4,11 @@
 
 echo "hello itc-136"
 echo "edited in github"
+echo "This is your current working directory:"
+pwd
+
+echo "This is the version of linux you're running:"
+uname -a
+
+echo "These are your logged in users:"
+who
