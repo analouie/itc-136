5a98153700b6e806b9ab2dbcaac83c37c3ef0e6d
Analia Arredondo <analia.ayla@gmail.com>
2018-12-09 18:33:50 -0800

    Update filefinder.sh

--- filefinder.sh
+++ filefinder.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-printf "\n Please enter a file name "
+#Where did I put that file?: Write a script that utilizes locate, find, 
+#whereis and which to determine the location of a lost file and whether 
+#or not it is in the PATH, if it is an executable file.
+
+printf "\n Please enter a file name to locate: "
 read fname
 
-#find the file
-filefound=$( readlink -f $fname )
+#locate the file --------------------------------------------------------
+filelocated=$( locate $fname )
 
-#printf "\n Please enter the path to check "
-#read path2check
+#whereis the file -------------------------------------------------------
+filewhereis=$( whereis $fname )
 
+#which (executable file) ------------------------------------------------
+filewhich=$( which -f $fname )
 
-echo -en "\e[94m Your File is here: $filefound"
+#executable?
+#fileexecutable=$( -x "$fname" )
 
+#find the file ----------------------------------------------------------
+#[ ! -f "$fname" ]
+#filefound=$( find . -name $fname )
+#filefound=$( readlink -f $fname )
 
-#if find $path2check -name $fname -print -quit |
-   #grep -q '^'; then
-  #echo "the file exists!"
-#else
-  #echo "the file does not exist!"
-#fi
+#finds files regardless of case "-iname"
+if find . -iname '$fname'  
+then
+    echo -e "\e[44m     File '${fname}' is located at: \e[39m ${filelocated} \e[0m"
+    echo -e "\e[45m     '${fname}' is : \e[39m ${filewhereis} \e[0m"
+         if which -a "$fname"  #$filewhich
+         then
+             echo -e "\e[42m     File '${fname}' is executable. \e[0m"
+         else
+             echo -e "\e[41m     File '${fname}' is not executable or found. \e[0m"
+         fi
+else
+    echo -e "\e[41m     File '${fname}' not found. \e[0m"
+fi
